9a70caed66aa7b78dc9dd45d2c4d7c8ecd973057
mrakhaf <muhammadrakhaf@gmail.com>
2022-10-14 21:26:20 +0700

    init

---
+++ handler/book.go
@@ -0,0 +1,59 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"pustaka-api/book"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
+)
+
+func RootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Muhammad",
+		"bio":  "I love code!",
+	})
+}
+
+func BooksHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
+	})
+}
+
+func QueryHandler(c *gin.Context) {
+	id := c.Query("title")
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": id,
+	})
+}
+
+func UploadBooksHandler(c *gin.Context) {
+	var bookInput book.BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+
+	if err != nil {
+
+		errorMassages := []string{}
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMassage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
+			errorMassages = append(errorMassages, errorMassage)
+
+		}
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": errorMassages,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
+	})
+}
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"pustaka-api/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", handler.RootHandler)
+	router.GET("/books/:id", handler.BooksHandler)
+	router.GET("/query", handler.QueryHandler)
+	router.POST("/uploadbooks", handler.UploadBooksHandler)
+
+	router.Run()
+}
