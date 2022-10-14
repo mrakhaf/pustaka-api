4606118318dacdd571e0984c42ccc9e27c0f331d
mrakhaf <muhammadrakhaf@gmail.com>
2022-10-14 21:41:14 +0700

    up

--- book/entity.go
+++ book/entity.go
@@ -1,15 +1,7 @@
 package book
 
-// import (
-// 	"time"
-
-// 	"google.golang.org/protobuf/types/known/timestamppb"
-// )
-
-// // type Book struct {
-// // 	ID        int
-// // 	Title     string
-// // 	Price     int
-// // 	Rating    int
-// // 	CreatedAt timestamppb,
-// // }
+type Book struct {
+	ID    int
+	Title string
+	Price int
+}
